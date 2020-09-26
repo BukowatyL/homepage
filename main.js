bbce1da0cafa72b8c6c231026032927dedbf0de2
Łukasz Buczynski <lukasz.buczynski@networks.pl>
2020-09-26 12:12:43 +0200

    KickOff projectu

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName = 'Łukasz';
+const secondName = 'Buks';
+const age = 36;
+console.log('Witaj na mojej stronie')
+console.log(`Nazywam się: ${firstName}`);
+console.log(`Mam: ${age} lat`);
+alert(`Witaj na mojej stronie. Nazywam się:${firstName} ${secondName}`);
